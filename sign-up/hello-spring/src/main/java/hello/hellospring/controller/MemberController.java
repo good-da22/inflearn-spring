86c3e9e5d1e3fb8e564b5feaa16e1ce79d030213
TaeheeGu <71616468+TaeheeGu@users.noreply.github.com>
2022-03-21 15:32:46 +0900

    홈,등록,조회 화면 및 컨트롤러 생성

--- sign-up/hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
+++ sign-up/hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,9 +1,15 @@
 package hello.hellospring.controller;
 
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller //컴포넌트 스캔 방식
 public class MemberController {
@@ -19,5 +25,27 @@ public class MemberController {
     @Autowired //스프링 컨테이너에서 연결, 의존관계 주입, 생성자 주입(권장)
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new") //Get 메소드, 주로 조회시
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new") //Post 메소드, 주로 등록시
+    public String creat(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/member_list";
     }
 }
