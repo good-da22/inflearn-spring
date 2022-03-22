180028df3ec9111b8c9bbfbe8438a348a7df401b
TaeheeGu <71616468+TaeheeGu@users.noreply.github.com>
2022-03-22 12:49:47 +0900

    JDBC Template 추가

--- sign-up/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ sign-up/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,6 +1,6 @@
 package hello.hellospring;
 
-import hello.hellospring.repository.JdbcMemberRepository;
+import hello.hellospring.repository.JdbcTemplateMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,6 +30,7 @@ public class SpringConfig {
     public MemberRepository memberRepository() { //인터페이스를 두고 구현체 바꾸기, 기존 코드 수정 필요 없음!
         //다형성을 활용, 개방-폐쇄 원칙(OCP) 기존 코드를 손대지 않고 설정만으로 구현 클래스 변경 가능
 //        return new MemoryMemberRepository();
-        return new JdbcMemberRepository(dataSource); //Config 파일 변경 만으로 재구성 가능
+//        return new JdbcMemberRepository(dataSource); //Config 파일 변경 만으로 재구성 가능
+        return new JdbcTemplateMemberRepository(dataSource);
     }
 }
